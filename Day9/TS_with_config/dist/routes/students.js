507f5429421ac23f4a66d51e581b46471fa119d3
Arnav Gupta <arnav@codingblocks.com>
2018-04-26 11:11:20 +0530

    tsconfig and build scripts
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Day9/TS_with_config/dist/routes/students.js
@@ -0,0 +1,12 @@
+const route = require('express').Router();
+const students = [];
+route.get('/', (req, res) => res.json(students));
+route.post('/', (req, res) => {
+    students.push({
+        name: req.body.name,
+        grade: req.body.grade,
+        age: parseInt(req.body.age)
+    });
+    res.json(students[students.length - 1]);
+});
+module.exports = route;
