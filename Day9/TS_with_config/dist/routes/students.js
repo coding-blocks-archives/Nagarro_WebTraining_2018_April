10b403bbff3ed84387910048c97c6864826a7ecf
Arnav Gupta <arnav@codingblocks.com>
2018-04-26 12:26:28 +0530

    typescript classes
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Day9/TS_with_config/dist/routes/students.js
+++ Day9/TS_with_config/dist/routes/students.js
@@ -1,18 +1,22 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const route = express_1.default.Router();
+const express_1 = require("express");
+const route = express_1.Router();
+class Student {
+    constructor(name, grade, age) {
+        this.name = name;
+        this.grade = grade;
+        this.age = age;
+    }
+    isAdult() {
+        return this.age > 18;
+    }
+}
 const students = [];
 route.get('/', (req, res) => res.json(students));
 route.post('/', (req, res) => {
-    students.push({
-        name: req.body.name,
-        grade: req.body.grade,
-        age: parseInt(req.body.age)
-    });
+    const body = req.body;
+    students.push(new Student(req.body.name, req.body.grade, parseInt(req.body.age)));
     res.json(students[students.length - 1]);
 });
 exports.default = route;
--- Day9/TS_with_config/src/routes/students.ts
+++ Day9/TS_with_config/src/routes/students.ts
@@ -1,17 +1,39 @@
-import express from 'express'
+import express, {Router, Request} from 'express'
 
-const route = express.Router()
 
-const students = []
+const route = Router()
+
+class Student {
+  public name: string
+  public grade: string
+  public age: number
+
+  constructor(name: string, grade: string, age: number) {
+    this.name = name
+    this.grade = grade
+    this.age = age
+  }
+  isAdult (): boolean {
+    return this.age > 18
+  }
+}
+
+
+interface RequestBody {
+  [k: string]: string
+}
+
+const students: Array<Student> = []
 
 route.get('/', (req, res) => res.json(students))
 
-route.post('/', (req, res) => {
-  students.push({
-    name: req.body.name,
-    grade: req.body.grade,
-    age: parseInt(req.body.age)
-  })
+route.post('/', (req: Request, res) => {
+  const body: RequestBody = req.body
+  students.push(new Student(
+    req.body.name,
+    req.body.grade,
+    parseInt(req.body.age)
+  ))
   res.json(students[students.length - 1])
 })
 
